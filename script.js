0192b2bc8cbf5e9b979c7d2db415d8790e9c83d6
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-17 17:31:18 +0200

    Implement dynamic image card generation and update card styles for improved layout

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-for (let i = 0; i < 6; i++) {
-    fetch("https://lanciweb.github.io/demo/api/pictures/")
-        .then(response => response.json())
-        .then(data => {
-        })
+console.log("Photo Blog");
+const row = document.querySelector(".row");
+console.log(row);
 
-}
+const endPoint = ("https://lanciweb.github.io/demo/api/pictures/")
+
+
+// for (let i = 0; i < 6; i++) {
+fetch(endPoint)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(picture => {
+            const { title, url } = picture;
+            console.log(title, url);
+            const picturesMarkUpStr = `
+            <div class="col-4">    
+                <div class="card">
+                    <h2 class="card_title">${title}</h2>
+                    <div class="card_img">
+                        <img src="${url}" alt="">
+                    </div>
+                    <div class="card_body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            `
+            row.insertAdjacentHTML("beforeend", picturesMarkUpStr)
+
+        });
+
+    })
+    .catch(error => {
+        console.error(error);
+    })
+//}
